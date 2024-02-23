6dba8092d955228bbc4e000ee0bddbfc7fba3dbb
Emrehrz <horuzemre3@gmail.com>
2024-02-23 10:24:16 +0300

    first commit

---
+++ frontend/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <nav class="flex gap-14">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
---
+++ frontend/frontend/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import PostView from '@/views/PostView.vue'
+import CategoryView from '@/views/CategoryView.vue'
+import TagView from '@/views/TagView.vue'
+import AllCategoriesView from '@/views/AllCategoriesView.vue'
+import AllTagsView from '@/views/AllTagsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: "/category",
+      name: "Category",
+      component: CategoryView
+    },
+    {
+      path: "/tag",
+      name: "Tag",
+      component: TagView
+    },
+    {
+      path: "/post",
+      name: "Post",
+      component: PostView
+    },
+    {
+      path: "/categories",
+      name: "Categories",
+      component: AllCategoriesView
+    },
+    {
+      path: "/tags",
+      name: "Tags",
+      component: AllTagsView
+    },
+
+  ]
+})
+
+
+export default router
